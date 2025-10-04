1d00dc9b672dc0a819701f61441e9330a46a896a
paulohenriquesantos222-pixel <paulohenriquesantos222@gmail.com>
2025-10-03 21:08:03 -0300

    teste Postman

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Middleware para interpretar o corpo das requisições como JSON
+
 app.use(express.json());
 
-// Importando as rotas
+
 const jogosRouter = require('./routes/jogos');
-// CORRIGIDO: de 'desenvolvedoras' para 'desenvolvedor' para corresponder ao nome do arquivo
+
 const desenvolvedorasRouter = require('./routes/desenvolvedores'); 
-// CORRIGIDO: de 'plataformas' para 'plataforma' para corresponder ao nome do arquivo
+
 const plataformasRouter = require('./routes/plataforma'); 
 const clientesRouter = require('./routes/clientes');
 const pedidosRouter = require('./routes/pedidos');
